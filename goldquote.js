a640f3666db0e3a04a23327c8b277cb05b8d62e9
Benjamin Geißler <b.geissler89@gmail.com>
2015-12-07 19:49:42 +0100

    Update goldquote.js

--- goldquote.js
+++ goldquote.js
@@ -1,18 +1,26 @@
-/* Copyright (c) 2014 Ron Litzenberger, MIT License */
+/* Benjamin Geißler -> Forked from Ron Litzenberger, MIT License */
+
+
 "use strict";
 
 function goldquote(done){
-  	var request = require('request');
-  	var cheerio = require('cheerio');
-	request('http://www.goldpriceoz.com/', function (error, response, html) {
-  	if (!error && response.statusCode == 200) {
-     	var $ = cheerio.load(html);
-    	var a=$('div.gold_rate').each(function(i, element){
+    var request = require('request');
+    var cheerio = require('cheerio');
+  request('http://www.goldpriceoz.com/gold-price-europe/', function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var a=$('b.gold_font').each(function(i, element){
     	//if (i===18)
     	 return this.nodeType == 3;
-		}).text();
-		done(a);
-		}
+		  }).text();
+      var regGold = /.*\ ([0-9]{1,4}\.\d{2})\ \ Euro/ig
+		  var result = regGold.exec(a);
+      if (result) {
+        done({ounce: parseFloat(result[1]), gramm: parseFloat(result[1]/28.3495) });
+		  }else{
+        done(null)
+      }
+    }
 	});
 }
 module.exports = goldquote;
