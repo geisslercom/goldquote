3b4caf3060beb8632768f990c1892fef10bf284b
Benjamin Geißler <b.geissler89@googlemail.com>
2016-06-13 11:19:09 +0200

    structre update

--- goldquote.js
+++ goldquote.js
@@ -3,24 +3,28 @@
 
 "use strict";
 
-function goldquote(done){
-    var request = require('request');
-    var cheerio = require('cheerio');
-  request('http://www.goldpriceoz.com/gold-price-europe/', function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      var a=$('b.gold_font').each(function(i, element){
-    	//if (i===18)
-    	 return this.nodeType == 3;
-		  }).text();
-      var regGold = /.*\ ([0-9]{1,4}\.\d{2})\ \ Euro/ig
-		  var result = regGold.exec(a);
-      if (result) {
-        done({ounce: parseFloat(result[1]), gramm: parseFloat(result[1]/31.1034768) });
-		  }else{
-        done(null)
-      }
-    }
-	});
+function goldquote(callback, err) {
+    let request = require('request');
+    let cheerio = require('cheerio');
+    
+    request('http://www.goldpriceoz.com/gold-price-europe/', function(error, response, html) {
+        if (!error && response.statusCode == 200) {
+            let $ = cheerio.load(html);
+            let a = $('.gold-span span:nth-child(3) b').each(function(i, element) {
+                //if (i===18)
+                return this.nodeType == 3;
+            }).text();
+            let regGold = /.*\ ([0-9]{1,4}\.\d{2})\ \ Euro/ig
+            let result = regGold.exec(a);
+            if (result) {
+                callback({
+                    ounce: parseFloat(result[1]),
+                    gramm: parseFloat(result[1] / 31.1034768)
+                });
+            } else {
+                callback(null)
+            }
+        }
+    });
 }
 module.exports = goldquote;
